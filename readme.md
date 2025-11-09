a4fbbccbafcdedef813d7b3b0915758d983f4d7d
Max <mc.cui@mail.utoronto.ca>
2025-11-09 14:48:10 -0500

    we got bigger mangos to mustard

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Mango Mustard
+> "We've got bigger mangos to mustard lil bro"
+
+## Six Seven
+![SixSeven](https://preview.redd.it/the-emote-became-real-side-by-side-comparsion-v0-2ovank9l1fxf1.png?width=1080&crop=smart&auto=webp&s=569594a30102a5972c8c830ed5a0553117e35ca6)
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <raylib.h>
+
+int main(void) {
+    const int screenWidth = 960;
+    const int screenHeight = 720;
+
+    InitWindow(screenWidth, screenHeight, "MangoMustard");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
